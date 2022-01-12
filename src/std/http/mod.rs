54a8a8053b7c42d948471f88f48a218d889297c6
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-01-12 22:27:21 +0800

    add dep

--- src/std/http/mod.rs
+++ src/std/http/mod.rs
@@ -1,2 +1,10 @@
 pub mod server;
 pub mod client;
+
+pub struct Request {
+    inner: http::Request<Box<dyn std::io::Read>>,
+}
+
+pub struct Response {
+    inner: http::Response<Box<dyn std::io::Write>>,
+}
