56f0e9f65e33db92e0f6f9749321975aa851cfce
zhuxiujie <zhuxiujia@qq.com>
2022-01-13 14:21:48 +0800

    add closer

--- src/std/http/mod.rs
+++ src/std/http/mod.rs
@@ -1,10 +1,17 @@
+use std::io::{Read, Write};
+use crate::std::io::Closer;
+
 pub mod server;
 pub mod client;
 
+pub trait ReadCloser: Read + Closer {}
+
+pub trait WriteCloser: Write + Closer {}
+
 pub struct Request {
-    inner: http::Request<Box<dyn std::io::Read>>,
+    inner: http::Request<Box<dyn ReadCloser>>,
 }
 
 pub struct Response {
-    inner: http::Response<Box<dyn std::io::Write>>,
+    inner: http::Response<Box<dyn WriteCloser>>,
 }
